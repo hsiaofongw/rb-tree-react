a9571241deab9fec1b6f0cc12a3bc5a698468f76
hsiaofongw <hsiaofong.w@gmail.com>
2022-12-31 18:14:10 +0800

    update

--- src/rb-tree/delete.ts
+++ src/rb-tree/delete.ts
@@ -1,3 +1,4 @@
+import { getMax, getMin } from "./find";
 import { NodeToNode, TreeNode } from "./types";
 import {
   isRed,
@@ -55,5 +56,49 @@ export const deleteMax = <KeyT, ValueT>(
     }
 
     return deleteRight(unSplit(root));
+  }
+};
+
+const getSuccessorLeft = <KeyT, ValueT>(
+  root: NonNullable<TreeNode<KeyT, ValueT>>
+): TreeNode<KeyT, ValueT> => {
+  const lhsMax = getMax(root.left) as NonNullable<typeof root>;
+  lhsMax.left = deleteMax(root.left);
+  lhsMax.right = root.right;
+  lhsMax.isRed = root.isRed;
+  return reconcile(lhsMax);
+};
+
+const getSuccessorRight = <KeyT, ValueT>(
+  root: NonNullable<TreeNode<KeyT, ValueT>>
+): TreeNode<KeyT, ValueT> => {
+  const rhsMin = getMin(root.right) as NonNullable<typeof root>;
+  rhsMin.right = deleteMin(root.right);
+  rhsMin.left = root.left;
+  rhsMin.isRed = root.isRed;
+  return reconcile(rhsMin);
+};
+
+export const deleteRootNode = <KeyT, ValueT>(
+  root: TreeNode<KeyT, ValueT>
+): TreeNode<KeyT, ValueT> => {
+  if (root?.left && root?.right) {
+    if (isRed(root.left)) {
+      return getSuccessorLeft(root);
+    } else if (isRed(root.right)) {
+      return getSuccessorRight(root);
+    } else if (isRed(root.left.left)) {
+      return getSuccessorLeft(root);
+    } else if (isRed(root.right.left)) {
+      return getSuccessorRight(root);
+    } else {
+      return getSuccessorLeft(unSplit(root) as any);
+    }
+  } else if (root?.left) {
+    root.left.isRed = root.isRed;
+    return root.left;
+  } else if (root?.right) {
+    root.right.isRed = root.isRed;
+    return root.right;
   }
 };
