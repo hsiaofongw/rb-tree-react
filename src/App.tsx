ed5a2d1533c79df21ca8ea4b2c58732da7c20057
hsiaofongw <hsiaofong.w@gmail.com>
2023-01-01 01:59:18 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,39 @@ import "./App.css";
 import { Box } from "@mui/material";
 import ts, { factory } from "typescript";
 
-function utf8_to_b64(str: string) {
-  return window.btoa(unescape(encodeURIComponent(str)));
-}
-
-function createDataUrl(str: string) {
-  return `data:text/javascript,${str}`;
-}
-
 function App() {
   useEffect(() => {
-    const prevAnchor = window.document.getElementById("123");
-    if (prevAnchor) {
-      prevAnchor.remove();
+    const fileContent = "export default 1234;";
+    const fileName = "myScript.js";
+    const fileObject = new File([fileContent], fileName, {
+      type: "text/javascript",
+    });
+    const myScriptUrl = URL.createObjectURL(fileObject);
+    const scriptImportMapEle = window.document.querySelector(
+      "script[type=importmap]"
+    );
+    const moduleAlias = "myScripts";
+    if (scriptImportMapEle) {
+      try {
+        if (scriptImportMapEle.textContent) {
+          const mapObject = JSON.parse(scriptImportMapEle.textContent);
+          if (mapObject && mapObject.imports) {
+            if (mapObject.imports[moduleAlias] === myScriptUrl) {
+              return;
+            }
+            mapObject.imports[moduleAlias] = myScriptUrl;
+            scriptImportMapEle.textContent = JSON.stringify(mapObject);
+            window.setTimeout(() => {
+              import("./" + fileName).then((res) => {
+                console.log("load:", res);
+              });
+            });
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
     }
-    const scriptEle = window.document.createElement("script");
-    scriptEle.setAttribute("id", "123");
-    scriptEle.text = `
-      import("./myFile.js").then((res) => {
-        console.log("Res:", res.default);
-      });
-    `;
-    window.document.head.appendChild(scriptEle);
   }, []);
 
   return <Box>Hello</Box>;
