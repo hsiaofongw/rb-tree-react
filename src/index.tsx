8fcb8b22aab2cab80d795a5665cef629465391f2
hsiaofongw <hsiaofong.w@gmail.com>
2023-01-02 15:19:33 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout } from "./Layout";
